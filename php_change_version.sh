762da34bb8cf6c59a8f0c5b32aa2ec272fc0b4e5
Cristea Ioan <ioan.cristea@pitechnologies.ro>
2021-07-10 17:28:56 +0300

    Script to change php versions. Usage(PHP 7.2->7.3): php_change_version 2 3

---
+++ php_change_version.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+php -v && sudo a2dismod php7.$1 && sudo a2enmod php7.$2 && sudo service apache2 restart && sudo update-alternatives --set php /usr/bin/php7.$2 && sudo update-alternatives --set phar /usr/bin/phar7.$2 && sudo update-alternatives --set phar.phar /usr/bin/phar.phar7.$2 && sudo update-alternatives --set phpize /usr/bin/phpize7.$2 && sudo update-alternatives --set php-config /usr/bin/php-config7.$2 && sudo systemctl restart apache2 && php -v
